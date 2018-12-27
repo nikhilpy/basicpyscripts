0e61e162f5d88e43937dd46fe336734c49863e58
nikhilpy <nikhil.belalekar@gmail.com>
2018-12-27 14:58:37 +0530

    Github initialization shell script

--- git.sh
+++ git.sh
@@ -4,7 +4,8 @@ git config --global user.name "nikhilpy"
 
 git config --global user.email "nikhil.belalekar@gmail.com"
 
-cd basicpyscripts
-
 git clone https://github.com/nikhilpy/basicpyscripts.git
+
+cd basicscripts
+
 git pull
