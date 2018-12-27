37936da75911ea6da5e93660ce18681e25791a5a
nikhilpy <nikhil.belalekar@gmail.com>
2018-12-27 14:48:43 +0530

    added the shell script file for initializing github bash

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+git config --global user.name "nikhilpy"
+
+git config --global user.email "nikhil.belalekar@gmail.com"
+
+cd basicpyscripts
+
+git clone https://github.com/nikhilpy/basicpyscripts.git
+git pull
